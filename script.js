cadc6f719b31fe1d230311dc0e2768663522254d
Fred-Hunter <fredski.1+fredgithub@hotmail.co.uk>
2023-10-01 01:11:55 +0100

    Mark svg2 against svg1 instead of other way

--- script.js
+++ script.js
@@ -5,25 +5,24 @@ svg1.style = "position: absolute; left: 0; top: 0";
 svg2.style = "position: absolute; left: 0; top: 0";
 
 svg1.position
-const numberOfPaths = svg1.childNodes[2].childNodes[0].childNodes.length;
-const svg2Path = document.getElementById(`path${numberOfPaths + 1}`);
+const svg1Paths = [...svg1.childNodes[2].childNodes[0].childNodes].filter(n => n.tagName === "path");
+const svg2Path = document.getElementById(`path${svg1Paths.length + 1}`);
 
 function measurePath() {
     let circles = "";
-    
-    for(let pathNumber = 1; pathNumber <= numberOfPaths; pathNumber++){
-        const path = document.getElementById(`path${pathNumber}`);
-        if (!path) break;
-
+            
+    const pathLength = svg2Path.getTotalLength();
+    for(let distance = 0; distance <= pathLength; distance += 5) {
+        const svg2Point = svg2Path.getPointAtLength(distance);
         
-        const pathLength = path.getTotalLength();
-        for(let distance = 0; distance <= pathLength; distance += 5) {
-            const point = path.getPointAtLength(distance);
-            const isOverlapping = svg2Path.isPointInStroke(point.matrixTransform(path.getTransformToElement(svg2Path)));
+        let isOverlapping = false; 
+        const svg2PointInSvg1Coords = svg2Point.matrixTransform(svg2Path.getTransformToElement(svg1));
 
-            if (isOverlapping) console.log(point);
-            circles += `<circle tabindex="0" fill="#fff" stroke="${isOverlapping ? "red" : "blue"}" cx="${point.x}" cy="${point.y}" r="0.5" />`;
-        }
+        svg1Paths.forEach(p => {
+            isOverlapping = isOverlapping || p.isPointInStroke(svg2PointInSvg1Coords);
+        });
+
+        circles += `<circle tabindex="0" fill="#fff" stroke="${isOverlapping ? "red" : "blue"}" cx="${svg2PointInSvg1Coords.x}" cy="${svg2PointInSvg1Coords.y}" r="0.5" />`;
     }
 
     document.getElementById("circles").innerHTML = circles;
@@ -34,4 +33,4 @@ SVGElement.prototype.getTransformToElement = SVGElement.prototype.getTransformTo
     return toElement.getScreenCTM().inverse().multiply(this.getScreenCTM());
 };
 
-svg1.addEventListener("load", (event) => measurePath());
+svg2.addEventListener("load", (event) => measurePath());
